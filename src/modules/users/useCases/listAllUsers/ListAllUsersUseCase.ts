7cae363ffbd390af936aad74b84b7f1c2791b881
Whalesson Ferreira <40179310+Whalesson-Ferreira@users.noreply.github.com>
2022-10-25 01:47:40 +0000

    Initial commit

---
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -0,0 +1,26 @@
+import { User } from "../../model/User";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  user_id: string;
+}
+
+class ListAllUsersUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  execute({ user_id }: IRequest): User[] {
+    const currentUser = this.usersRepository.findById(user_id);
+
+    if (!currentUser) {
+      throw new Error("Non existing user");
+    }
+    if (!currentUser.admin) {
+      throw new Error("Non admin user");
+    }
+    const usersList = this.usersRepository.list();
+
+    return usersList;
+  }
+}
+
+export { ListAllUsersUseCase };
